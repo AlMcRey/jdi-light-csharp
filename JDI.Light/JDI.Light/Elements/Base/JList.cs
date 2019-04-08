04db751693ce28eb58ee5eed247506fc2a5e53e0
Aleksandr Seregin <aleksandr_seregin@epam.com>
2019-04-08 15:59:41 +0300

    Initial

---
+++ JDI.Light/JDI.Light/Elements/Base/JList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace JDI.Light.Elements.Base
+{
+    public class JList<T>
+    {
+        protected List<T> elements = new List<T>();
+
+        public JList() {}
+
+        public JList(IEnumerable<T> elements)
+        {
+            this.elements.AddRange(elements);
+        }
+
+        
+    }
+}
